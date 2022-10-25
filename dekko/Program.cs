1260c5eea40eb94500916711a8fc8ef5768b0b50
Michael Griffin <mpgriffin5@gmail.com>
2022-10-24 20:42:57 -0700

    Initial commit

---
+++ dekko/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace dekko
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string init = "init";
+
+            var validCommands = new HashSet<string> { init };
+
+            if (args == null)
+            {
+                Console.WriteLine("args is null");
+                return;
+            }
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("args is empty");
+                return;
+            }
+
+            var command = args[0];
+
+            if (!validCommands.Contains(command))
+            {
+                Console.WriteLine($"Unknown command: {command}");
+                return;
+            }
+
+            switch (command)
+            {
+                case "init":
+                    Initialize();
+                    break;
+            }
+        }
+
+        //
+        //
+        private static void Initialize()
+        {
+            Console.WriteLine("* * * Welcome to dekko!!!! * * *");
+            Console.WriteLine();
+            Console.WriteLine("Initializing dekko repository");
+        }
+    }
+}
