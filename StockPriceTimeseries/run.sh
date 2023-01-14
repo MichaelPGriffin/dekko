7f24a6562fed664afc2dc5711b763c1871a51334
Michael Griffin <mpgriffin5@gmail.com>
2023-01-13 23:00:14 -0800

    Update filepaths in time-series shell script

--- StockPriceTimeseries/run.sh
+++ StockPriceTimeseries/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
-rm -rf ./data/
-mkdir data
-rm -rf ./responses/
-mkdir responses
-node ./src/main.js 
-paste ./responses/*.csv | awk '{print (NR==1?"#":NR-1) "\t" $0}' > ./data/closing-prices.tsv & echo 'CSVs are combined. See ./data/closing-prices.tsv'
+rm -rf /c/Users/Owner/Projects/dekko/StockPriceTimeseries/data/
+mkdir /c/Users/Owner/Projects/dekko/StockPriceTimeseries/data/
+rm -rf /c/Users/Owner/Projects/dekko/StockPriceTimeSeries/responses/
+mkdir /c/Users/Owner/Projects/dekko/StockPriceTimeSeries/responses/
+node /c/Users/Owner/Projects/dekko/StockPriceTimeseries/src/main.js 
+paste /c/Users/Owner/Projects/dekko/StockPricetimeseries/responses/*.csv | awk '{print (NR==1?"#":NR-1) "\t" $0}' > /c/Users/Owner/Projects/dekko/StockPriceTimeseries/data/closing-prices.tsv & echo 'CSVs are combined. See ./data/closing-prices.tsv'
