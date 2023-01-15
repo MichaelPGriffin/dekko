a562626a4deae6986185f200072f3ea3b6888bc2
Michael Griffin <mpgriffin5@gmail.com>
2023-01-14 22:42:37 -0800

    Add parameter handling to `islands.js`

--- StockGraphAnalysis/islands.js
+++ StockGraphAnalysis/islands.js
@@ -14,8 +14,16 @@ const nodes = symbols.map(s => new Vertex(s));
 const getEdgeWeight = (v1, v2) => correlations.get(v1.Value, v2.Value);
 const graph = initializeFullyConnectedGraph(nodes, getEdgeWeight);
 
-const islands = discoverIslands(3, graph);
+const targetIslandCount = process.argv.slice(2)[0];
+
+if (!targetIslandCount || Number.isNaN(targetIslandCount) || targetIslandCount < 2) {
+  const messageText =  `Invalid count parameter value ${targetIslandCount}.\n` +
+    'Must be equal to 2 or greater';
+  throw new Error(messageText);
+}
+
+const islands = discoverIslands(targetIslandCount, graph);
 console.log('The islands are:');
 islands.forEach(v => console.log(v.Value));
 
-console.log('End');
+console.log();
