d0fbbc8316dde7fcec499af86264c9eebf1d6496
Michael Griffin <mpgriffin5@gmail.com>
2023-01-15 21:21:04 -0800

    Implement `branch current` command and outline other commands

--- dekko/Subcommands/Branch.cs
+++ dekko/Subcommands/Branch.cs
@@ -2,14 +2,47 @@
 {
     public static class Branch
     {
+        private static readonly string CurrentBranchPath = $@"{Constants.RefsPath}\current-branch";
+
         public static void Execute(string[] args)
         {
             CheckParameterValidity(args);
 
-            throw new NotImplementedException();
+            var command = args[1];
+
+            switch (command)
+            {
+                case "current":
+                    Current();
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
         }
 
         private static void CheckParameterValidity(string[] args)
+        {
+            if (args == null || args.Length < 2)
+            {
+                throw new ArgumentException("Invalid parameters passed to `branch` method");
+            }
+        }
+
+        public static void Current()
+        {
+            var currentBranchName = File.ReadAllLines(CurrentBranchPath).FirstOrDefault();
+            Console.WriteLine(currentBranchName);
+        }
+
+        public static void List(string[] args)
+        {
+        }
+
+        public static void New(string[] args)
+        {
+        }
+
+        public static void Remove(string[] args)
         {
         }
     }
