8c1993597926df050347bdb7555340b2aaf40749
Michael Griffin <mpgriffin5@gmail.com>
2023-01-15 21:19:36 -0800

    Bugfix: address mis-typed filepath

--- dekko/Constants.cs
+++ dekko/Constants.cs
@@ -14,6 +14,8 @@ namespace dekko
 
         public static readonly string RootPath = @"C:\Users\Owner\Projects\dekko";
 
-        public static readonly string RosterPath = $@"{RootPath}\.refs\roster";
+        public static readonly string RefsPath = $@"{RootPath}\.refs";
+
+        public static readonly string RosterPath = $@"{RefsPath}\roster";
     }
 }
