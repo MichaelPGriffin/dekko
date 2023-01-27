3c0b87fa41b9c120adbc16ff0bf3a76198ba2bdd
Michael Griffin <mpgriffin5@gmail.com>
2023-01-26 21:59:09 -0800

    Refactor method to anticipate collection of stock symbols

--- dekko/Subcommands/Subset.cs
+++ dekko/Subcommands/Subset.cs
@@ -21,8 +21,20 @@ namespace dekko.Subcommands
         // TODO: Consider deleting/hiding files in branch that don't make the golden cross indicator show TRUE.
         private static async Task PrintGoldenCrosDemonstrators()
         {
+            var symbols = new[] { "O" };
+
+            foreach (var symbol in symbols)
+            {
+                var timeSeries = await GetSymbolTimeSeries(symbol);
+                var goldenCrossCandidate = new GoldenCross(timeSeries);
+                var status = goldenCrossCandidate.IsTrue() ? "is a" : "is not a";
+                Console.WriteLine($"The symbol {symbol} {status} golden cross");
+            }
+        }
+
+        private static async Task<decimal[]> GetSymbolTimeSeries(string symbol)
+        {
             var currentBranch = Branch.GetCurrentBranchName();
-            var symbol = "O";
             var filePath = @$"{Constants.BranchStoragePath}\{currentBranch}\responses\{symbol}.csv";
             var rawData = await File.ReadAllLinesAsync(filePath);
 
@@ -31,11 +43,7 @@ namespace dekko.Subcommands
                 .Select(r => Convert.ToDecimal(r))
                 .ToArray();
 
-            var goldenCrossCandidate = new GoldenCross(timeSeries);
-
-            var status = goldenCrossCandidate.IsTrue() ? "is a" : " is not a";
-
-            Console.WriteLine($"The symbol {symbol} {status} golden cross");             
+            return timeSeries;
         }
     }
 }
